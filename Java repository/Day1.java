60010ac7fe53e02c79898550898f532634e25602
Nasir-2006 <n9750998@gmail.com>
2025-08-14 12:42:35 +0500

    Restart

--- Java repository/Day1.java
+++ Java repository/Day1.java
@@ -1,5 +1,23 @@
-
-
-public class Day1 {
-    
-}
+// import java.util.Scanner;
+// public class Day1 {
+//     public static void main(String[] args) {
+//         int rows,col;
+//         Scanner sc = new Scanner(System.in);
+//         rows = sc.nextInt();sc.nextLine();
+//         col = sc.nextInt();
+//         for(int i=1;i<=rows;i++)
+//         {
+//             for(int j=1;j<=col;j++)
+//             {
+//                 if(i==1||i==rows||j==1||j==col)
+//                 {
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
