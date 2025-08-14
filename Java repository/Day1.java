327acbc4a7f3d04d9194f58ceb4fa5ceafc673b9
Nasir-2006 <n9750998@gmail.com>
2025-08-14 13:30:37 +0500

    initial commit

--- Java repository/Day1.java
+++ Java repository/Day1.java
@@ -21,7 +21,7 @@
 //         }
 //     }
 // }
-//--------------------------------------------------------Right angle Tragle-----------------------------------------------
+//--------------------------------------------------------Right angle Triagle-----------------------------------------------
 // import java.util.Scanner;
 // public class Day1{
 //     public static void main(String[] args) {
@@ -58,5 +58,31 @@
 //             }
 //             System.out.println();
 //         }
+//     }
+// }
+//-------------------------------------------------Half Butterfly--------------------
+// import java.util.Scanner;
+// public class Day1 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i = 1;i<=n;i++)
+//         {
+//             for(int j = 1;j<=i;j++)
+//             {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         for(int i = 1;i<=n;i++)
+//         {
+//             for(int j = i ;j<=n;j++)
+//             {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
 //     }
 // }
