6748457c3f90daa677f8f2a320b1aa8574976e6d
Nasir-2006 <n9750998@gmail.com>
2025-08-14 13:14:48 +0500

    Restart(1.0)

--- Java repository/Day1.java
+++ Java repository/Day1.java
@@ -21,3 +21,42 @@
 //         }
 //     }
 // }
+//--------------------------------------------------------Right angle Tragle-----------------------------------------------
+// import java.util.Scanner;
+// public class Day1{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int n = sc.nextInt();
+//         for(int i = 1;i<=n;i++)
+//         {
+//             for(int j=1;j<=i;j++)
+//             {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+//------------------------------------------------------Right angle Triagle(Descreaing order)-----------------------------
+// import java.util.Scanner;
+// public class Day1 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int n = sc.nextInt();
+//         for(int i = 1; i<=n;i++)
+//         {
+//             for(int k = 1;k<=i-1;k++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             for(int j = i;j<=n;j++)
+//             {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
