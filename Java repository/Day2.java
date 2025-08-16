5247dd1abf4a56d7bfbeb81879513681307b0122
Nasir-2006 <n9750998@gmail.com>
2025-08-16 18:59:37 +0500

    initial commit

--- Java repository/Day2.java
+++ Java repository/Day2.java
@@ -32,30 +32,30 @@
 //         System.out.print(pro);
 //     }
 // }
-import java.util.Scanner;
-import java.util.Arrays;
+// import java.util.Scanner;
+// import java.util.Arrays;
 
-public class Day2 {
-    public static int[] reverse(int[] arr) {
-        // Write your code below
-        int[] rev = new int[arr.length];
-        for(int i = 0;i<arr.length;i++)
-        {
-            rev[i] = arr[arr.length-i-1];
-        }
-       return rev;
-    }
+// public class Day2 {
+//     public static int[] reverse(int[] arr) {
+//         // Write your code below
+//         int[] rev = new int[arr.length];
+//         for(int i = 0;i<arr.length;i++)
+//         {
+//             rev[i] = arr[arr.length-i-1];
+//         }
+//        return rev;
+//     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        String[] stringArr = text.split(",");
-        int[] arr = new int[stringArr.length];
-        for (int i = 0; i < stringArr.length; i++) {
-            arr[i] = Integer.parseInt(stringArr[i]);
-        }
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         String text = scanner.nextLine();
+//         String[] stringArr = text.split(",");
+//         int[] arr = new int[stringArr.length];
+//         for (int i = 0; i < stringArr.length; i++) {
+//             arr[i] = Integer.parseInt(stringArr[i]);
+//         }
         
-        int[] result = reverse(arr);
-        System.out.println("The reversed array is: " + Arrays.toString(result));
-    }
-}
+//         int[] result = reverse(arr);
+//         System.out.println("The reversed array is: " + Arrays.toString(result));
+//     }
+// }
