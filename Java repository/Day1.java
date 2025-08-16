ad4d83fc33289437502ffd43b7e66fc53f385e06
Nasir-2006 <n9750998@gmail.com>
2025-08-16 13:26:24 +0500

    initial commit

--- Java repository/Day1.java
+++ Java repository/Day1.java
@@ -28,11 +28,12 @@
 //         Scanner sc = new Scanner(System.in);
 //         System.out.println("Enter num:");
 //         int n = sc.nextInt();
+//         int sum=0;
 //         for(int i = 1;i<=n;i++)
 //         {
 //             for(int j=1;j<=i;j++)
 //             {
-//                 System.out.print("*");
+//             System.out.print(j);
 //             }
 //             System.out.println();
 //         }
@@ -85,4 +86,45 @@
 //         }
 
 //     }
+// }
+//-------------------------------------------------------Solid Rohombus----------------------------------------------------
+// import java.util.Scanner;
+// public class Day1 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int n = sc.nextInt();
+//         for(int i = 1;i<=n;i++)
+//         {
+//             for(int j = 1;j<=n-i;j++)
+//             {
+//                 System.out.print(" ");
+//             }
+//             for(int k = 1;k<=n;k++)
+//             {
+//                 System.out.print("*");
+//             }System.out.println();
+//         }
+//     }
+// }
+//----------------------------------------------------------Piramid of Number--------------------------------------------
+// import java.util.Scanner;
+// public class Day1 {
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int k = 1;k<=n-i;k++)
+//         {
+//             System.out.print(" ");
+//         }
+//         for(int j = 1;j<=i;j++)
+//         {
+//             System.out.print(i+" ");
+//         }System.out.println();
+//     }
+// }
+    
 // }
---
+++ Java repository/Day2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Day2{
+    public static void main(String[] args) {
+        int[] number = {5,3,8,9,1};
+        Arrays.sort(number);
+        System.out.print(Arrays.toString(number));
+    }
+}
