bbd864731d68a09879a3a017d5aed4103160fe64
Nasir-2006 <n9750998@gmail.com>
2025-08-19 15:18:38 +0500

    initial commit

---
+++ Day5.java
@@ -0,0 +1,74 @@
+// import java.util.Scanner;
+// class Day5{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int num1 = sc.nextInt();sc.nextLine();
+//         String bina = Integer.toBinaryString(num1);
+//         System.out.println("Binary is "+bina);
+//         int dec1=Integer.parseInt(bina,2);
+//         System.out.println("Decimal is "+dec1);
+//         System.out.println("Enter num2:");
+//         int num2 = sc.nextInt();sc.nextLine();
+//         String binar = Integer.toBinaryString(num2);
+//         System.out.println("Binary is "+binar);
+//         int dec2 = Integer.parseInt(binar,2);
+//         System.out.println("Decimal is "+dec2);
+//         int res = dec1 | dec2;
+//         System.out.println("Bitwise AND is "+res);
+//     }
+// }
+// import java.util.Scanner;
+// public class Day5 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int num = sc.nextInt();sc.nextLine();
+//         for(int i=1;i<=10;i++)
+//         {
+//             System.out.println(num+"*"+i+" = "+num*i);
+//         }
+//     }
+// }
+// import java.util.Scanner;
+// public class Day5 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int num = sc.nextInt();sc.nextLine();
+//         int sum = 0;
+//         for(int i = 1;i <= num;i++)
+//         {
+//             if(i%2==1)
+//             {
+//                 sum+=i;
+//             }
+//         }
+//         System.out.println(sum);
+//     }
+// }
+// import java.util.Scanner;
+// public class Day5 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int num = sc.nextInt();
+//         int fac=1;
+//         for(int i =1;i<=num;i++)
+//         {
+//             fac*=i;
+//         }
+//         System.out.println(fac);
+//     }
+// }
+import java.util.Scanner;
+public class Day5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
