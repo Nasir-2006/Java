92acc26946d0fcff385b1fd3e1fbfa93fd74c9d6
Nasir-2006 <n9750998@gmail.com>
2025-08-21 22:18:34 +0500

    initial committ

--- Day5.java
+++ Day5.java
@@ -82,22 +82,67 @@
 //         System.out.print(rem);
 //     }
 // }
+// import java.util.Scanner;
+// public class Day5 {
+//     public static int HCF(int num1,int num2) {
+//         while(num1!=0)
+//         {
+//             int temp = num2;
+//             num2 = num1%num2;
+//             num1 = temp;
+//         }
+//         return num1;
+//     }
+// public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter num1:");int num1 = sc.nextInt();
+//     System.out.println("Enter num2:");int num2 = sc.nextInt();
+//     int hcf = HCF(num1,num2);
+//     System.out.println(hcf);
+// }
+// }
+// import java.util.Scanner;
+// public class Day5 {
+// public static int HCF(int num1,int num2)
+// {
+// while(num2!=0)
+//         {
+//             int temp = num2;
+//             num2 = num1%num2;
+//             num1 = temp;
+//         }
+//         return num1;
+// } 
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num1:");
+//         int num1 = sc.nextInt();sc.nextLine();
+//         System.out.println("Enter num2:");
+//         int num2 = sc.nextInt();sc.nextLine();
+//         int div = HCF(num1,num2);
+//         int LCM = (num1*num2)/div;
+//         System.out.println(LCM);
+//     }
+// }
 import java.util.Scanner;
 public class Day5 {
-    public static int HCF(int num1,int num2) {
-        while(num1!=0)
-        {
-            int temp = num2;
-            num2 = num1%num2;
-            num1 = temp;
-        }
-        return num1;
+public static int rev(int num) {
+    int rev=0,rem=0;
+    while(num!=0)
+    {
+        rem = num%10;
+        rev = rev*10+rem;
+        num /=10;
     }
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter num1:");int num1 = sc.nextInt();
-    System.out.println("Enter num2:");int num2 = sc.nextInt();
-    int hcf = HCF(num1,num2);
-    System.out.println(hcf);
+    // System.out.println(rev);
+    return rev;
 }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num:");
+        int num = sc.nextInt();
+        int num2 = rev(num);
+        if(num==num2)
+        System.out.println("Number is palindrome");
+    }
 }
