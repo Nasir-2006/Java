05dd3026d6d7af8cec2f90e1cb92bc6878068958
Nasir-2006 <n9750998@gmail.com>
2025-08-22 19:49:11 +0500

    initial commit

--- Day5.java
+++ Day5.java
@@ -124,25 +124,35 @@
 //         System.out.println(LCM);
 //     }
 // }
-import java.util.Scanner;
-public class Day5 {
-public static int rev(int num) {
-    int rev=0,rem=0;
-    while(num!=0)
-    {
-        rem = num%10;
-        rev = rev*10+rem;
-        num /=10;
-    }
-    // System.out.println(rev);
-    return rev;
-}
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter num:");
-        int num = sc.nextInt();
-        int num2 = rev(num);
-        if(num==num2)
-        System.out.println("Number is palindrome");
-    }
-}
+// import java.util.Scanner;
+// public class Day5 {
+// public static int rev(int num) {
+//     int rev=0,rem=0;
+//     while(num!=0)
+//     {
+//         rem = num%10;
+//         rev = rev*10+rem;
+//         num /=10;
+//     }
+//     System.out.println(rev);
+//     return rev;
+// }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num:");
+//         int num = sc.nextInt();
+//         int num2 = rev(num);
+//         if(num==num2)
+//         System.out.println("Number is palindrome");
+//     }
+// }
+// import java.util.Scanner;
+// public class Day5 {
+//     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter num:");
+//        int num = sc.nextInt();
+//        int dig = (int)Math.log10(num)+1;
+//        System.out.println(dig);
+//     }
+// }
