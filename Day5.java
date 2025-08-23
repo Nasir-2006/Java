4f49c184e81a4a9d6df7a3569d5577412b7ac3d8
Nasir-2006 <n9750998@gmail.com>
2025-08-23 10:06:54 +0500

    initial commit

--- Day5.java
+++ Day5.java
@@ -178,3 +178,34 @@
 //     }
 // }
 // }
+// import java.util.Scanner;
+// public class Day5 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner (System.in);
+//         System.out.println("Enter num:");
+//         int num = sc.nextInt();
+//         int num1 = 0,num2 = 1;
+//         while(num1 <= num)
+//         {
+//             System.out.print(num1+" ");
+//             int temp = num1+num2;
+//             num1 = num2;
+//             num2 = temp;
+            
+//         }
+//     }
+// }
+// import java.util.Scanner;
+// public class Day5 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter num1:");
+//         int num1 = sc.nextInt();
+//         System.out.println("Enter num2:");
+//         int num2 = sc.nextInt();
+//         num1 = num1+num2-(num2=num1);
+//         System.out.println("Swapped num are "+num1+" "+num2);
+//     }
+// }
