b0e3c7d9670ada83052301069f0eb5bd57a07996
Nasir-2006 <n9750998@gmail.com>
2025-08-27 09:40:28 +0500

    initial commit

---
+++ Java repository/OOP.java
@@ -0,0 +1,62 @@
+class car{
+   float fuel;
+   int air;
+   int gear;
+   public void addfuel(float fuel)
+   {
+     this.fuel = fuel;
+   }
+   public void fuelInCar(){
+    if(this.fuel==0)
+    {
+        System.out.println("Car cannot drive bcz there is no fuel");
+    }
+    else
+    {
+        System.out.println("Car has started");
+        this.fuel--;
+        System.out.println("fuel now is:"+this.fuel);
+    }
+   }
+   public void airInCar(int air){
+    if(air==0){
+        System.out.println("Cannot drive car bcz there is no air in tires fill them will 30");
+    }
+    else if(air>30)
+    {
+        System.out.println("Reduce air to 30");
+    }
+    else
+    {
+        System.out.println("There is air in tires");
+    }
+   }
+   public void cargear(int gear)
+   {
+    if(gear==0)
+    {
+        System.out.println("Car is neutral");
+    }
+    else if(gear<=1||gear>=5)
+    {
+        System.out.println("You are all good to go");
+    }
+    else
+    {
+        System.out.println("Invalid gear");
+    }
+   }
+}
+public class OOP {
+public static void main(String[] args) {
+    car alto = new car();
+    alto.addfuel(30);
+    alto.fuelInCar();
+    alto.airInCar(32);
+    alto.cargear(0);
+    alto.fuelInCar();
+    alto.airInCar(32);
+    alto.cargear(1);
+
+}
+}
