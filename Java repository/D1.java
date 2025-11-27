4bc6f60906ee3e647cb95375800126564752ebe4
Nasir-2006 <n9750998@gmail.com>
2025-11-27 11:08:37 +0500

    junk

--- Java repository/D1.java
+++ Java repository/D1.java
@@ -3,8 +3,9 @@ public class D1{
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
+        System.out.printf("Enter your name:");
         String name = sc.nextLine();
+        System.out.printf("Enter ");
         System.out.println("Your name is "+ name );
         sc.close();
         
