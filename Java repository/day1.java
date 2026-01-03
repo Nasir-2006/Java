71224f9c0593cfdd0f134e37c0ec819ede51f021
Nasir-2006 <n9750998@gmail.com>
2026-01-03 17:51:50 +0500

    Start

---
+++ Java repository/day1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day1 {
+    
+    public static void main(String arg [])
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter name:");
+    String name = sc.nextLine();sc.nextInt();
+    }
+
+}
